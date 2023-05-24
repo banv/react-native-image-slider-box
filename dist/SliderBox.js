34bd54bd6df1fc3b7a345fddc9208b2f88705d55
banv <banv@ai-t.vn>
2023-05-24 14:45:09 +0700

    using source properties

--- dist/SliderBox.js
+++ dist/SliderBox.js
@@ -31,7 +31,7 @@ import styles from "./SliderBox.style";
 // firstItem = 0
 // activeOpacity
 // autoplayInterval = 3000
-// bearerToken
+// sourceProps
 
 const width = Dimensions.get("window").width;
 
@@ -80,15 +80,12 @@ export class SliderBox extends Component {
       imageLoadingColor = "#E91E63",
       underlayColor = "transparent",
       activeOpacity = 0.85,
-      bearerToken,
+      sourceProps= {},
     } = this.props;
 
     let imageSource = item;
     if (typeof item === "string" ) {
-      imageSource = { uri: item };
-      if (bearerToken) {
-        imageSource.headers = { Authorization: `Bearer ${bearerToken}` };
-      }
+      imageSource = { uri: item, ...sourceProps };
     }
 
     return (
